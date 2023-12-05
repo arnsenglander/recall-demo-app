d19ad89436fcecff4c1b516b1e18f6604fdccb55
Zaki <zakislaboratory@gmail.com>
2023-12-05 13:36:44 -0600

    Meeting List prototype

---
+++ client/src/components/MeetingList/MeetingList.tsx
@@ -0,0 +1,36 @@
+import * as ScrollArea from '@radix-ui/react-scroll-area';
+import './styles.css';
+import { Meeting } from '../../types/meeting';
+import { CalendarIcon } from '@radix-ui/react-icons';
+
+
+interface MeetingListProps {
+    meetings: Meeting[];
+    onSelectMeeting: (meeting: Meeting) => void;
+}
+
+const MeetingList = ({ meetings, onSelectMeeting }: MeetingListProps) => (
+  <ScrollArea.Root className="ScrollAreaRoot">
+    <ScrollArea.Viewport className="ScrollAreaViewport">
+      {meetings.map((meeting, index) => (
+        <div
+          key={index}
+          className="meetingItem"
+          onClick={() => onSelectMeeting(meeting)}
+        >
+          <CalendarIcon height={12} width={20} style={{ marginRight: 10 }} />
+          <div>{meeting.title}</div>
+        </div>
+      ))}
+    </ScrollArea.Viewport>
+    <ScrollArea.Scrollbar className="ScrollAreaScrollbar" orientation="vertical">
+      <ScrollArea.Thumb className="ScrollAreaThumb" />
+    </ScrollArea.Scrollbar>
+    <ScrollArea.Scrollbar className="ScrollAreaScrollbar" orientation="horizontal">
+      <ScrollArea.Thumb className="ScrollAreaThumb" />
+    </ScrollArea.Scrollbar>
+    <ScrollArea.Corner className="ScrollAreaCorner" />
+  </ScrollArea.Root>
+);
+
+export default MeetingList;
