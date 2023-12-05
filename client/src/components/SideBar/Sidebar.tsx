68722e03a7ce36bb2b6ea7ba81e0bc91434f1bef
Zaki <zakislaboratory@gmail.com>
2023-12-05 14:40:04 -0600

    sidebar

--- client/src/components/SideBar/Sidebar.tsx
+++ client/src/components/SideBar/Sidebar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import MeetingList from '../MeetingList/MeetingList';
 import { Meeting } from '../../types/meeting';
 import './styles.css';
+import NewBotModal from '../NewBotModal/NewBotModal';
+import { Separator } from '@radix-ui/react-separator';
 
 interface SidebarProps {
   meetings: Meeting[]; // Array of meeting titles
@@ -10,13 +12,24 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ meetings, onSelectMeeting, onSendBot }) => {
+
+  const [modalOpen, setModalOpen] = React.useState(false);
+
   return (
     <aside className="sidebar">
       <div className="sidebarTitle">Meetings</div>
+      <Separator className="sidebarSeparator" color="black" />
       <div className="sidebarList">
         <MeetingList meetings={meetings} onSelectMeeting={onSelectMeeting} /> 
       </div>
-      <button className="newMeetingButton" onClick={onSendBot}>New Meeting</button>
+      <button 
+        className="newMeetingButton" 
+        onClick={() => setModalOpen(true)}
+      >New Bot</button>
+      
+      <NewBotModal 
+        open={modalOpen} onOpenChange={() => setModalOpen(!modalOpen)} 
+        onSendBot={() => onSendBot()} />
     </aside>
   );
 };
