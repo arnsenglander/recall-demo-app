a4770d7f0fbf99f1a1413a5c14e6de01760928d1
Zaki <zakislaboratory@gmail.com>
2023-12-05 14:39:58 -0600

    new bot modal

--- client/src/components/NewBotModal/NewBotModal.tsx
+++ client/src/components/NewBotModal/NewBotModal.tsx
@@ -1,4 +1,5 @@
 import * as Dialog from '@radix-ui/react-dialog';
+import { Cross2Icon } from '@radix-ui/react-icons';
 import './styles.css';
 
 interface NewBotModalProps {
@@ -9,19 +10,35 @@ interface NewBotModalProps {
 
 const NewBotModal = ({ open, onOpenChange, onSendBot }: NewBotModalProps) => (
   <Dialog.Root open={open} onOpenChange={onOpenChange}>
-    <Dialog.Overlay className="overlay" />
-    <Dialog.Content className="content">
-      <div className="title">Send Bot to Meeting</div>
-      <div className="description">
-        The bot will join the meeting at the provided URL.
-        A transcription and action items will be generated automatically.
-      </div>
-      <Dialog.Close className="close" />
-      <div className="buttons">
-        <button className="cancel" onClick={onOpenChange}>Cancel</button>
-        <button className="send" onClick={onSendBot}>Confirm</button>
-      </div>
-    </Dialog.Content>
+    <Dialog.Portal>
+      <Dialog.Overlay className="DialogOverlay" />
+      <Dialog.Content className="DialogContent">
+        <Dialog.Title className="DialogTitle">New Bot</Dialog.Title>
+        <Dialog.Description className="DialogDescription">
+            The bot will join the meeting at the provided URL and automatically transcribe the meeting.
+        </Dialog.Description>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="meeting_url">
+            Meeting URL
+          </label>
+          <input className="Input" id="name" placeholder="Zoom, Google Meet, etc." /> 
+        </fieldset>
+        <fieldset className="Fieldset">
+          <label className="Label" htmlFor="name">
+            Bot Name
+          </label>
+          <input className="Input" id="username" placeholder="Transcription Bot" />
+        </fieldset>
+        <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end', gap: '1rem' }}>
+          <Dialog.Close asChild>
+            <button className="Button grey">Cancel</button>
+            </Dialog.Close>
+          <Dialog.Close asChild>
+            <button className="Button">Confirm</button>
+          </Dialog.Close>
+        </div>
+      </Dialog.Content>
+    </Dialog.Portal>
   </Dialog.Root>
 );
 
