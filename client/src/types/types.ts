18efaf1e9a8da93c1420ecd91003bed54d0884a5
Zaki <zakislaboratory@gmail.com>
2023-12-05 20:23:36 -0600

    types

--- client/src/types/types.ts
+++ client/src/types/types.ts
@@ -1,5 +1,12 @@
 export interface Meeting {
     id: string;
-    title: string;
-    actionItems: string[];
+    recall_bot_id: string;
+    meeting_url: string;
+    updatedAt: string;
+    createdAt: string;
+}
+
+export interface CreateBotRequest {
+    meeting_url: string;
+    name?: string;
 }
