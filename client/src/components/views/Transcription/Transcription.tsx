c2ec67f9eb732219de7bc84ff31061dd518091d5
Zaki <zakislaboratory@gmail.com>
2023-12-05 20:23:52 -0600

    transcription component

--- client/src/components/views/Transcription/Transcription.tsx
+++ client/src/components/views/Transcription/Transcription.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import { Meeting } from '../../../types/meeting';
+import { Meeting } from '../../../types/types';
 import './styles.css';
+import { Transcription } from '../../../lib/transcribe';
 
 interface TranscriptionViewProps {
     meeting: Meeting;
-    transcription: string;
+    transcription: Transcription;
 }
 
 const TranscriptionView: React.FC<TranscriptionViewProps> = ({ meeting, transcription }) => {
   return (
     <div className="view">
-      <h2>{meeting.title}</h2>
-      <p><strong>Transcription:</strong> {transcription}</p>
+      <h2>{meeting.meeting_url}</h2>
+      <p><strong>Transcription:</strong> {transcription.asString()}</p>
       <ul>
-        <strong>Action Items:</strong>
-        {meeting.actionItems.map((actionItem, index) => (
-          <li key={index}>{actionItem}</li>
+        <strong>Segments:</strong>
+        {transcription.getSegments.map((segment, index) => (
+          <li key={index}>{segment.asString()}</li>
         ))}
       </ul>
       {/* Add more details as needed */}
